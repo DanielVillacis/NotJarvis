c01b9c2cab5c555710e77c798abba705b8b9302a
Daniel Villacis <daniel1997v@gmail.com>
2022-02-07 12:12:17 -0500

    Correction de la structure du BooleanNot

--- src/jarvis/atoms/primitives/booleans/BooleanPrimitiveNot.java
+++ src/jarvis/atoms/primitives/booleans/BooleanPrimitiveNot.java
@@ -5,51 +5,54 @@ import java.util.ArrayList;
 import jarvis.atoms.AbstractAtom;
 import jarvis.atoms.BoolAtom;
 import jarvis.atoms.ObjectAtom;
+import jarvis.atoms.primitives.PrimitiveOperationAtom;
 import jarvis.interpreter.JarvisInterpreter;
 
-public class BooleanPrimitiveNot extends BooleanPrimitiveOperation {
+public class BooleanPrimitiveNot extends PrimitiveOperationAtom {
 	
 	//OPERATIONSPRIMITIVES
 	/*
-	* Cette classe implante la partie de l'opération primitive spécifique au '!' appelée : NOT ou Négation logique.
+	* Cette classe implante la partie de l'opï¿½ration primitive spï¿½cifique au '!' appelï¿½e : NOT ou Nï¿½gation logique.
+	* 
+	* Traitement special pour le not "!". Cette classe herite de PrimitiveOperationAtom directement.
+	* Elle possede ses propres methodes pour le traitement booleen d'un seul atome passee en argument dans Jarvis.
 	* 
 	*/
 	
-	@Override
 	public String makeKey() {
 		return "BooleanPrimitiveNot";
 	}
 	
-	// Le nombre d'argument doit etre 0 (seulement self)
+	// Le nombre d'argument doit etre -1 pour ne pas en tenir compte(seulement self).
     protected void init() {
-        argCount = 0;
+        argCount = -1;
     }
 
-	// Comme la négation prend seulement un argument et la methode execute de la classe BooleanPrimitiveOperation 
-	// attends recevoir un deuxième argument, il est nécessaire de réecrire la methode 'execute' pour quelle aie 
-	// seulement un argument afin de calculer le résultat de la négation.
-	
-	@Override
-	protected AbstractAtom execute(JarvisInterpreter ji,ObjectAtom self) {
-		
-		//Ici, on peut assumer que l'objet qui a reçu le message (self) est un int et possède donc
-		//le champ "value". 
-		BoolAtom bool1 = (BoolAtom) self.message("bool");
-		
-		//Procède finalement au calcul spécifique à chaque primitive concernant 1 bool.	
-		return calculateResult(ji, bool1);
-	}
+	// Comme la nï¿½gation prend seulement un argument et la methode execute de la classe BooleanPrimitiveOperation 
+	// attends recevoir un deuxiï¿½me argument, il est nï¿½cessaire de rï¿½ecrire la methode 'execute' pour quelle aie 
+	// seulement un argument afin de calculer le rï¿½sultat de la nï¿½gation.
 	
 	protected AbstractAtom calculateResult(JarvisInterpreter ji, BoolAtom bool1) {
-		// C'est ici que l'opération réelle a lieu
+		// C'est ici que l'opï¿½ration rï¿½elle a lieu
 		boolean result = !bool1.getValue();
 		
 		// Ici, construit un objet bool manuellement
-		// À noter, on retourne un objet de type bool, pas un atome de type
+		// ï¿½ noter, on retourne un objet de type bool, pas un atome de type
 		// bool.
 		ArrayList<AbstractAtom> data = new ArrayList<AbstractAtom>();
 		data.add(new BoolAtom(result));
 		
 		return new ObjectAtom(((ObjectAtom) ji.getEnvironment().get("bool")), data, ji);
 	}
+    
+	protected AbstractAtom execute(JarvisInterpreter ji,ObjectAtom self) {
+		
+		//Ici, on peut assumer que l'objet qui a reï¿½u le message (self) est un int et possï¿½de donc
+		//le champ "value". 
+		BoolAtom bool1 = (BoolAtom) self.message("value");
+		
+		//Procï¿½de finalement au calcul spï¿½cifique ï¿½ chaque primitive concernant 1 bool.	
+		return calculateResult(ji, bool1);
+	}
+	
 }
